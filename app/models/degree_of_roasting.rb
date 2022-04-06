c9283984835a1ea6a87f386d79a76c21c69b09ac
torako-san <gldemo1207@gmail.com>
2022-04-06 14:32:11 +0900

    Active Hash「degree_of_roasting」の作成

---
+++ app/models/degree_of_roasting.rb
@@ -0,0 +1,17 @@
+class DegreeOfRoasting_ < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'ミディアムロースト（浅煎り）' },
+    { id: 3, name: 'ハイロースト（中浅煎り）' },
+    { id: 4, name: 'シティロースト（中煎り）' },
+    { id: 5, name: 'フルシティロースト（中深煎り）' },
+    { id: 6, name: 'フレンチロースト（深煎り）' },
+    { id: 7, name: 'イタリアンロースト（深煎り）' },
+    { id: 8, name: 'ライトロースト（浅煎り）' },
+    { id: 9, name: 'シナモンロースト（浅煎り）' },
+    { id: 10, name: '生豆' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :shops
+end
