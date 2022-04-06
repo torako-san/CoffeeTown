844dc3aca77231b0ffa2c48d68b5f14914219cd2
torako-san <gldemo1207@gmail.com>
2022-04-06 10:30:56 +0900

    テーブル情報をdbに反映

--- db/migrate/20220406003545_create_productions.rb
+++ db/migrate/20220406003545_create_productions.rb
@@ -1,7 +1,22 @@
 class CreateProductions < ActiveRecord::Migration[6.0]
   def change
     create_table :productions do |t|
-
+      t.string  :name,                         null: false
+      t.date    :purchase_date,                null: false
+      t.string  :coffee_beans_type
+      t.string  :farm_name
+      t.text    :product_report
+      t.integer :product_category_id,          null: false
+      t.integer :coffee_blend_id,              null: false
+      t.integer :degree_of_roasting_id,        null: false
+      t.integer :country_of_origin_id,         null: false
+      t.integer :carefully_selected_method_id, null: false
+      t.integer :sweetness_id,                 null: false
+      t.integer :acidity_id,                   null: false
+      t.integer :bitter_taste_id,              null: false
+      t.integer :flavor_id,                    null: false
+      t.references :shop, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
