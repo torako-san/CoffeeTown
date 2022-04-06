170ba74a992cfc8f917982221f344191d886a4aa
torako-san <gldemo1207@gmail.com>
2022-04-06 18:26:32 +0900

    都道府県のリスト追加

--- app/models/prefectures.rb
+++ app/models/prefectures.rb
@@ -54,7 +54,8 @@ class Prefectures < ActiveHash::Base
     { id: 45, name: '大分県' },
     { id: 46, name: '宮崎県' },
     { id: 47, name: '鹿児島県' },
-    { id: 48, name: '沖縄県' }
+    { id: 48, name: '沖縄県' },
+    { id: 49, name: 'オンライン' }
   ]
 
   include ActiveHash::Associations
