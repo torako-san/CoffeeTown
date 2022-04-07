0ea827abce0c6481e6bb4f9b3de6b7e581429e4e
torako-san <gldemo1207@gmail.com>
2022-04-07 18:09:14 +0900

    データをテーブルに保存する処理の記述

--- app/models/shop_production.rb
+++ app/models/shop_production.rb
@@ -1,6 +1,6 @@
 class ShopProduction
   include ActiveModel::Model
-  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures_id, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :shop_id, :image
+  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :shop_id, :image
 
   with_options presence: true do
     validates :shop_name
@@ -20,5 +20,10 @@ class ShopProduction
     validates :acidity, numericality: { message: "can't be blank"}
     validates :bitter_taste, numericality: { message: "can't be blank"}
     validates :flavor, numericality: { message: "can't be blank"}
+  end
+
+  def save
+    shop = Shop.create(shop_name: shop_name, shi_ku_gun: shi_ku_gun, chome_banchi: chome_banchi, shop_url: shop_url, prefectures: prefectures, user_id: user_id)
+    Productions.create(production_name: production_name, purchase_date: purchase_date, coffee_beans_type: coffee_beans_type, farm_name: farm_name, product_report: product_report, product_category: product_category, coffee_blend: coffee_blend, degree_of_roasting: degree_of_roasting, country_of_origin: country_of_origin, carefully_selected_method: carefully_selected_method, sweetness: sweetness, acidity: acidity, bitter_taste: bitter_taste, flavor: flavor, shop_id: shop_id, user_id: user_id)
   end
 end
