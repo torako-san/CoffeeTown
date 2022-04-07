bc8ab2f2749124711a3f12516b726d7125ffb5c1
torako-san <gldemo1207@gmail.com>
2022-04-07 17:20:23 +0900

    扱いたいカラム名を記述

---
+++ app/models/shop_production.rb
@@ -0,0 +1,4 @@
+class ShopProduction
+  include ActiveModel::Model
+  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures_id, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :shop_id  
+end
