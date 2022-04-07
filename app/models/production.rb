ace73097a472a16a4375233ccba63e63be35846b
torako-san <gldemo1207@gmail.com>
2022-04-07 17:50:50 +0900

    商品モデルのアソシエーションを作成

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,2 +1,13 @@
 class Production < ApplicationRecord
+  belongs_to :bitter_taste
+  belongs_to :flavor
+  belongs_to :shop
+  belongs_to :product_category
+  belongs_to :coffee_blend
+  belongs_to :degree_of_roasting
+  belongs_to :country_of_origin
+  belongs_to :carefully_selected_method
+  belongs_to :sweetness
+  belongs_to :acidity
+  has_one_attached :image
 end
