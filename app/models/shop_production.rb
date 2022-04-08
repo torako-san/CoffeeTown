d896917830c31809e5db6a4ff9ed8d19d62b148b
torako-san <gldemo1207@gmail.com>
2022-04-08 15:05:35 +0900

    imageのバリテーションを変更

--- app/models/shop_production.rb
+++ app/models/shop_production.rb
@@ -1,24 +1,23 @@
 class ShopProduction
   include ActiveModel::Model
-  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category, :coffee_blend, :degree_of_roasting, :country_of_origin, :carefully_selected_method, :sweetness, :acidity, :bitter_taste, :flavor, :shop_id, :images
+  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures_id, :user_id, :production_name, :purchase_date, :coffee_beans_type_id, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :image
 
   with_options presence: true do
     validates :shop_name
     validates :shop_url
     validates :production_name
     validates :purchase_date
-    validates :images
+    validates :image
+    validates :user_id
   end
 
-  validates :images, length: { minimum: 1, maximum: 3, message: "は1枚以上3枚以下にしてください" }, allow_blank: true
-
-  with_options numericality: {other_than: 1, message: "can't be blank"}  do
-    validates :prefectures 
-    validates :product_category
-  end
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :product_category_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    shop = Shop.create(shop_name: shop_name, shi_ku_gun: shi_ku_gun, chome_banchi: chome_banchi, shop_url: shop_url, prefectures: prefectures, user_id: user_id)
-    Productions.create(production_name: production_name, purchase_date: purchase_date, coffee_beans_type: coffee_beans_type, farm_name: farm_name, product_report: product_report, product_category: product_category, coffee_blend: coffee_blend, degree_of_roasting: degree_of_roasting, country_of_origin: country_of_origin, carefully_selected_method: carefully_selected_method, sweetness: sweetness, acidity: acidity, bitter_taste: bitter_taste, flavor: flavor, shop_id: shop_id, user_id: user_id)
+    shop = Shop.create(shop_name: shop_name, shi_ku_gun: shi_ku_gun, chome_banchi: chome_banchi, shop_url: shop_url, prefectures_id: prefectures_id, user_id: user_id)
+    Productions.create(production_name: production_name, purchase_date: purchase_date, coffee_beans_type_id: coffee_beans_type_id, farm_name: farm_name, product_report: product_report, product_category_id: product_category_id,
+      coffee_blend_id: coffee_blend_id, degree_of_roasting_id: degree_of_roasting_id, country_of_origin_id: country_of_origin_id, carefully_selected_method_id: carefully_selected_method_id, sweetness_id: sweetness_id,
+      acidity_id: acidity_id, bitter_taste_id: bitter_taste_id, flavor_id: flavor_id, shop_id: shop.id, user_id: user_id)
   end
 end
