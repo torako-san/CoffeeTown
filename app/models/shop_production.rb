9cf40901918351c22a7f1d8ec07ca3b5012bbf5b
torako-san <gldemo1207@gmail.com>
2022-04-08 13:11:55 +0900

    imgeをimagesに変更

--- app/models/shop_production.rb
+++ app/models/shop_production.rb
@@ -1,13 +1,16 @@
 class ShopProduction
   include ActiveModel::Model
-  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category, :coffee_blend, :degree_of_roasting, :country_of_origin, :carefully_selected_method, :sweetness, :acidity, :bitter_taste, :flavor, :shop_id, :image
+  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category, :coffee_blend, :degree_of_roasting, :country_of_origin, :carefully_selected_method, :sweetness, :acidity, :bitter_taste, :flavor, :shop_id, :images
 
   with_options presence: true do
     validates :shop_name
     validates :shop_url
     validates :production_name
     validates :purchase_date
+    validates :images
   end
+
+  validates :images, length: { minimum: 1, maximum: 3, message: "は1枚以上3枚以下にしてください" }, allow_blank: true
 
   with_options numericality: {other_than: 1, message: "can't be blank"}  do
     validates :prefectures 
