79d3f26916ee5ab59e9e8d11eb0c12a96768da49
torako-san <gldemo1207@gmail.com>
2022-04-08 10:58:47 +0900

    共通モデルの処理をshosコントローラーに記載

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,6 +5,21 @@ class ShopsController < ApplicationController
   end
 
   def new
-    @shop = Shop.new
+    @shop_production = ShopProduction.new
   end
+
+  def create
+    @shop_production = ShopProduction.new(shop_params)
+    if @shop_production.valid?
+      @shop_production.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def shop_params
+    params.require(:shop_production).permit(:shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category, :coffee_blend, :degree_of_roasting, :country_of_origin, :carefully_selected_method, :sweetness, :acidity, :bitter_taste, :flavor).merge(user_id: current_user.id)
+  end
+  
 end
