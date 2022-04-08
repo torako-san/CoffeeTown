b9ce4111fbd08fba64f63cc68461e63957fa8a76
torako-san <gldemo1207@gmail.com>
2022-04-08 13:02:06 +0900

    未選択時の表示を変更

--- app/models/carefully_selected_method.rb
+++ app/models/carefully_selected_method.rb
@@ -1,11 +1,10 @@
 class CarefullySelectedMethod < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '不明' },
     { id: 2, name: 'ナチュラル（非水洗式）' },
     { id: 3, name: 'ウォッシュド（水洗式）' },
     { id: 4, name: 'ハニープロセス（パルプドナチュラル）' },
-    { id: 5, name: 'スマトラ式' },
-    { id: 6, name: '不明' }
+    { id: 5, name: 'スマトラ式' }
   ]
 
   include ActiveHash::Associations
