3a2784a314e1246cc117e22651ef061facc9bf42
torako-san <gldemo1207@gmail.com>
2022-04-08 12:18:27 +0900

    共通モデルの取り扱いたい情報名の変更

--- app/models/shop_production.rb
+++ app/models/shop_production.rb
@@ -1,6 +1,6 @@
 class ShopProduction
   include ActiveModel::Model
-  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :shop_id, :image
+  attr_accessor :shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures, :user_id, :production_name, :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category, :coffee_blend, :degree_of_roasting, :country_of_origin, :carefully_selected_method, :sweetness, :acidity, :bitter_taste, :flavor, :shop_id, :image
 
   with_options presence: true do
     validates :shop_name
