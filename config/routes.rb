6c71a441ac69e871111992f72cf88ae9f5200fc5
torako-san <gldemo1207@gmail.com>
2022-04-11 13:57:37 +0900

    productionコントローラーをshopsにネスと

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
-  resources :shops, only: [:index, :new, :create]
+  resources :shops, only: [:index, :new, :create] do
+    resources :production, only: [:index]
+  end
 end
