145895ac4ec719a83948d0bab40b34960e65fffb
torako-san <gldemo1207@gmail.com>
2022-04-13 09:40:09 +0900

    shopsコントローラーにshowを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
-  resources :shops, only: [:index, :new, :create] do
+  resources :shops, only: [:index, :new, :create, :show] do
     resources :production, only: [:index]
   end
 end
