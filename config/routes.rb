425b696303ad0faa3297a2d9cb31a399c3107e59
torako-san <gldemo1207@gmail.com>
2022-04-13 16:04:06 +0900

    productionsのshowのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
   resources :shops, only: [:index, :new, :create, :show] do
-    resources :production, only: [:index]
+    resources :production, only: [:index, :show]
   end
 end
