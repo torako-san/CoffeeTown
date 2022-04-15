3c6e059a7332fb946707659e46ef4e9bdbd57bfc
torako-san <gldemo1207@gmail.com>
2022-04-15 14:45:50 +0900

    showとeditのメソッドを統合

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,6 @@
 class ShopsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_shop, only: [:edit, :show]
 
   def index
     @shop = Shop.all.order('created_at DESC')
@@ -20,13 +21,16 @@ class ShopsController < ApplicationController
   end
 
   def show
-    @shop = Shop.find(params[:id])
   end
 
   def edit
   end
 
   private
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
   def shop_params
     params.require(:shop_production).permit(:shop_name, :shi_ku_gun, :chome_banchi, :shop_url, :prefectures_id, :production_name,
        :purchase_date, :coffee_beans_type, :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, 
