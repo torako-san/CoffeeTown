f41a3260a819ef71c933133c60fb7661dbed0864
torako-san <gldemo1207@gmail.com>
2022-04-15 14:58:55 +0900

    updateアクションをコントローラーに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
-  resources :shops, only: [:index, :new, :create, :show, :edit] do
+  resources :shops, only: [:index, :new, :create, :show, :edit, :update] do
     resources :productions, only: [:show]
   end
 end
