0256749a133704aa1771b8b464e4659e568d7206
torako-san <gldemo1207@gmail.com>
2022-04-15 14:11:39 +0900

    shopsにeditをルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
-  resources :shops, only: [:index, :new, :create, :show] do
+  resources :shops, only: [:index, :new, :create, :show, :edit] do
     resources :productions, only: [:show]
   end
 end
