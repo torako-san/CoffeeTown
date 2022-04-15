49b742ad39a297717497d35b4e18e38b9420d299
torako-san <gldemo1207@gmail.com>
2022-04-15 15:49:50 +0900

    shopモデルにバリテーションを設定

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,4 +5,10 @@ class Shop < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefectures
+
+  with_options presence: true do
+    validates :shop_name
+  end
+
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
 end
