b487757891a9acac29046664f6e70230211f5907
torako-san <gldemo1207@gmail.com>
2022-04-18 11:39:57 +0900

    DBに保存されるカラム名を指定

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -20,4 +20,10 @@ class ProductionsController < ApplicationController
     @production = Production.find(params[:shop_id])
   end
 
+  def production_data_params
+    params.require(:production).permit(:production_name,  :purchase_date, :coffee_beans_type, :farm_name, :product_report,
+       :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, 
+       :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, :image ).merge(user_id: current_user.id)
+  end
+
 end
