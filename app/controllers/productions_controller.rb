099275f60d921ace5923d15ac0f1ca792b8bf091
torako-san <gldemo1207@gmail.com>
2022-04-18 11:29:27 +0900

    メソッドの同じ記述の可読性を向上させる記述

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,8 +1,15 @@
 class ProductionsController < ApplicationController
+  before_action :set_production, only: [:edit, :show]
+
   def edit
   end
 
   def show
+  end
+
+  private
+  def set_production
     @production = Production.find(params[:shop_id])
   end
+
 end
