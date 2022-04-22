d77ce1834625f7b8a05142dd5fda8a98e882ac49
torako-san <gldemo1207@gmail.com>
2022-04-22 14:51:07 +0900

    余白調整

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -8,7 +8,7 @@ class ProductionsController < ApplicationController
   end
 
   def update
-    if @production.update(production_data_params)
+    if @production.update(production_params)    
       redirect_to shop_production_path
     else
       render :edit
@@ -23,10 +23,10 @@ class ProductionsController < ApplicationController
     @production = Production.find(params[:id])
   end
 
-  def production_data_params
-    params.require(:production).permit(:production_name,  :purchase_date, :coffee_beans_type, :farm_name, :product_report,
-       :product_category_id, :coffee_blend_id, :degree_of_roasting_id, :country_of_origin_id, :carefully_selected_method_id, 
-       :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, {images: []}).merge(user_id: current_user.id)
+  def production_params
+    params.require(:production).permit(:production_name,  :purchase_date, :coffee_beans_type,
+       :farm_name, :product_report, :product_category_id, :coffee_blend_id, :degree_of_roasting_id, 
+       :country_of_origin_id, :carefully_selected_method_id, :sweetness_id, :acidity_id, :bitter_taste_id, :flavor_id, images: []).merge(user_id: current_user.id)
   end
 
 end
