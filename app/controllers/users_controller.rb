7b2cb5509ffbdcbfd8603ab87df7c766102cf42c
torako-san <gldemo1207@gmail.com>
2022-05-05 15:06:37 +0900

    変数をインスタンス変数化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @shop = user.shops
-    @nickname = user.nickname
+    @user = User.find(params[:id])
+    @shop = @user.shops
   end
 end
