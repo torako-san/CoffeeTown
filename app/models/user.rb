4461ab5e6603e048d90715652fc31607ff4a4671
torako-san <gldemo1207@gmail.com>
2022-05-07 07:19:22 +0900

    フォロー側のアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   has_many :shops
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: follower_id
 end
