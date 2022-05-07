a3ca4dcfc9efc839923ec9b46376ea8c0cbeae6d
torako-san <gldemo1207@gmail.com>
2022-05-08 08:38:01 +0900

    createアクションにフォローする人、フォローされる人を保存

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,8 @@
 class RelationshipsController < ApplicationController
   def create
+    following = current_user.relationships.build(follower_id: params[:user_id])
+    following.save
+    redirect_to request.referrer || root_path
   end
 
   def destory
