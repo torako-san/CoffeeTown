83cb5e263ab9b6dc8911d6067d810c822bcbfd4a
torako-san <gldemo1207@gmail.com>
2022-05-08 08:47:50 +0900

    特定のフォローしているユーザーを取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,9 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users = user.followings
   end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
 end
