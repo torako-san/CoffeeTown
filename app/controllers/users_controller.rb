f1b60598d86027aec272b3200354b73a6defd0dd
torako-san <gldemo1207@gmail.com>
2022-05-08 08:43:52 +0900

    自分以外のユーザーの情報を取得する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @shop = @user.shops
   end
+
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
 end
