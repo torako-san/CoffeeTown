26475996347de76d0a64d5d8a33f73b160ab254b
torako-san <gldemo1207@gmail.com>
2022-05-08 08:42:04 +0900

    未ログインユーザーの権限を制限

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,5 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   def create
     following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
