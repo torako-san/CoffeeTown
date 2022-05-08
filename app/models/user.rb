31ac3fd8441505d8621ba6ca79849988ffdf3592
torako-san <gldemo1207@gmail.com>
2022-05-08 09:09:43 +0900

    構文の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_many :relationships, foreign_key: :following_id
   has_many :followings, through: :relationships, source: :follower
 
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
 end
