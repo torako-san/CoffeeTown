1a01706dac302917abf2b2ed810f9df068f7c0c0
torako-san <gldemo1207@gmail.com>
2022-05-08 09:34:07 +0900

    余白調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where.not(id: current_user.id)
   end
-  
+
   def followings
     user = User.find(params[:id])
     @users = user.followings
