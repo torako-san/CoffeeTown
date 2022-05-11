e16d18badf35c19fe4d762a08fb6ba8575a394a1
torako-san <gldemo1207@gmail.com>
2022-05-11 12:21:38 +0900

    wish_listモデル作成

---
+++ db/migrate/20220511031912_create_wish_lists.rb
@@ -0,0 +1,10 @@
+class CreateWishLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :wish_lists do |t|
+      t.integer :user_id
+      t.integer :production_id
+
+      t.timestamps
+    end
+  end
+end
