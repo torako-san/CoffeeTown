2c4b59433741ed1b73a978069a84bc27584b75ca
torako-san <gldemo1207@gmail.com>
2022-05-12 12:45:08 +0900

    before_actionで商品idを取得

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,4 +1,6 @@
 class WishListsController < ApplicationController
+  before_action :product_params_params
+
   def create
     WishList.create(user_id: current_user.id, production_id: params[:id])
     redirect_to shop_production_path(:shop_id)
@@ -7,5 +9,11 @@ class WishListsController < ApplicationController
   def destroy
     WishList.find_by(user_id: current_user.id, production_id: params[:id]).destroy
     redirect_to shop_production_path(:shop_id)
+  end
+
+  private
+  
+  def product_params
+    @production = Production.find(params[:id])
   end
 end
