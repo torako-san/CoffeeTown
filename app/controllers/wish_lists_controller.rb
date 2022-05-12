9a41c3b12caa138d8d7de066303489cd8131f846
torako-san <gldemo1207@gmail.com>
2022-05-12 09:11:53 +0900

    createアクションを設定

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,2 +1,6 @@
 class WishListsController < ApplicationController
+  def create
+    Wish_Lists.create(user_id: current_user.id, production_id: params[:id])
+    redirect_to production_path
+  end
 end
