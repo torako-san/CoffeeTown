84f6768941a477b49093bf2ff73f3344cd46d57d
torako-san <gldemo1207@gmail.com>
2022-05-12 09:23:31 +0900

    wishlistsコントロラーのdestroyルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :wish_lists, only: [:create]
+  resources :wish_lists, only: [:create, :destroy]
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get :followings, on: :member
