fea1267ebc299ebf216407cfe705623e6f5bbc47
torako-san <gldemo1207@gmail.com>
2022-05-12 09:22:13 +0900

    destroyアクションを設定

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,6 +1,11 @@
 class WishListsController < ApplicationController
   def create
     Wish_Lists.create(user_id: current_user.id, production_id: params[:id])
-    redirect_to production_path
+    redirect_to shop_production_path
+  end
+
+  def destroy
+    Wish_Lists.find_by(user_id: current_user.id, production_id: params[:id]).destroy
+    redirect_to shop_production_path
   end
 end
