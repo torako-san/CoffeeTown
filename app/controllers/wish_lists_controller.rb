44617319122d11edb8a8fa9ca45dd7d1ce7b7223
torako-san <gldemo1207@gmail.com>
2022-05-12 12:47:14 +0900

    同期用のredirectを削除

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,18 +1,16 @@
 class WishListsController < ApplicationController
-  before_action :product_params_params
+  before_action :product_params only: [:create, :destroy]
 
   def create
     WishList.create(user_id: current_user.id, production_id: params[:id])
-    redirect_to shop_production_path(:shop_id)
   end
 
   def destroy
     WishList.find_by(user_id: current_user.id, production_id: params[:id]).destroy
-    redirect_to shop_production_path(:shop_id)
   end
 
   private
-  
+
   def product_params
     @production = Production.find(params[:id])
   end
